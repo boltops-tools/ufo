998b5e71be0e6a17fd339cce5614b8b95a6f3c37
Tung Nguyen <tongueroo@gmail.com>
2017-04-16 13:47:03 -0700

    rid of ENV['REGION'], it interfers with proper usage of ~/.aws/config

--- lib/ufo/aws_services.rb
+++ lib/ufo/aws_services.rb
@@ -3,19 +3,15 @@ require 'aws-sdk'
 module Ufo
   module AwsServices
     def ecs
-      @ecs ||= Aws::ECS::Client.new(region: region)
+      @ecs ||= Aws::ECS::Client.new
     end
 
     def elb
-      @elb ||= Aws::ElasticLoadBalancingV2::Client.new(region: region)
+      @elb ||= Aws::ElasticLoadBalancingV2::Client.new
     end
 
     def ecr
-      @ecr ||= Aws::ECR::Client.new(region: region)
-    end
-
-    def region
-      ENV['REGION'] || 'us-east-1'
+      @ecr ||= Aws::ECR::Client.new
     end
   end
 end
