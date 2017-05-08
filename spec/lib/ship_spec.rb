81a340f7ba95eaf471aec547637cf0b15e231ce1
Tung Nguyen <tongueroo@gmail.com>
2017-05-07 18:52:57 -0700

    fix ship spec

--- spec/lib/ship_spec.rb
+++ spec/lib/ship_spec.rb
@@ -14,9 +14,9 @@ describe Ufo::Ship do
   let(:task_definition) { service }
   let(:ship) { Ufo::Ship.new(service, task_definition, options) }
 
-  context "single service id" do
+  context "hi-web-prod service" do
     let(:service) { "hi-web-prod" }
-    it "should create or update single service" do
+    it "should create or update service" do
       allow(ship).to receive(:process_single_service)
       ship.deploy
       expect(ship).to have_received(:process_single_service)
@@ -32,41 +32,11 @@ describe Ufo::Ship do
     end
 
     context "1 services found" do
-      it "should call update service 3 times" do
+      it "should call update service" do
         allow(ship).to receive(:find_ecs_service).and_return(ecs_service("hi-web-prod"))
         allow(ship).to receive(:update_service)
         ship.deploy
         expect(ship).to have_received(:update_service).exactly(1).times
-      end
-    end
-  end
-
-  context "multiple service pattern" do
-    let(:service) { "hi-.*-prod" }
-    it "should update multiple service that are found" do
-      allow(ship).to receive(:process_multiple_services)
-      ship.deploy
-      expect(ship).to have_received(:process_multiple_services)
-    end
-
-    context "0 services found" do
-      it "should not call update_service" do
-        allow(ship).to receive(:find_all_ecs_services)
-        allow(ship).to receive(:update_service)
-        ship.deploy
-        expect(ship).to_not have_received(:update_service)
-      end
-    end
-
-    context "3 services found" do
-      it "should call update service 3 times" do
-        allow(ship).to receive(:find_all_ecs_services).
-                          and_yield(ecs_service("hi-web-prod")).
-                          and_yield(ecs_service("hi-worker-prod")).
-                          and_yield(ecs_service("hi-clock-prod"))
-        allow(ship).to receive(:update_service)
-        ship.deploy
-        expect(ship).to have_received(:update_service).exactly(3).times
       end
     end
   end
