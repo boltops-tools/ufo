e035703f8e5271e1ccb98feec663d6a1f5079f65
Tung Nguyen <tongueroo@gmail.com>
2017-06-11 11:07:38 -0700

    improve ufo ships help

--- lib/ufo/cli/help.rb
+++ lib/ufo/cli/help.rb
@@ -58,21 +58,21 @@ EOL
 
         def ships
 <<-EOL
-Builds docker image, registers it and ships it to multiple services.  So it ships the same docker image to multiple ECS services.
+Builds docker image, registers it and ships it to multiple services.  This deploys the same docker image to multiple ECS services.
 
 Examples:
 
 $ ufo ships hi-web-prod hi-clock-prod hi-worker-prod
 
-By convention the task and service names match for each of the services you ship. If you need override to this convention then you can specific the tasks with a special syntax.  In the special syntax the service and task definition is separate by a colon.  Example:
+By convention the task definition and service names match for each of the services you ship. If you need to override to this convention then you can specify the task definition for each service with a special syntax.  In the special syntax the service and task definition is separated by a colon.  Example:
 
 $ ufo ships hi-web-prod-1:hi-web-prod hi-clock-prod-1 hi-worker-prod-1
 
-Here ufo will ship to the hi-web-prod-1 ECS Service and use the hi-web-prod task definition, but use the convention for the rest of the service.
+Here ufo will deploy to the hi-web-prod-1 ECS Service using the hi-web-prod task definition, but use the convention for the rest of the service.
 
 For each service being deployed to, ufo will create the ECS service if the service does not yet exist on the cluster.  The deploy process will prompt you for the ELB `--target-group` if you are deploying to a 'web' service that does not yet exist.  Ufo determines that it is a web service by the name of the service. If the service has 'web' in the name then it is considered a web service. If it is not a web service then the `--target-group` option gets ignored.
 
-The prommpt can be bypassed by specifying a valid `--target-group` option or specifying the `---no-elb-prompt` option.
+The prommpt can be bypassed by specifying a valid `--target-group` option or specifying the `---no-elb-prompt` option.  Examples:
 
 $ ufo ships hi-web-prod hi-clock-prod hi-worker-prod --target-group arn:aws:elasticloadbalancing:us-east-1:123456789:targetgroup/hi-web-prod/jsdlfjsdkd
 
