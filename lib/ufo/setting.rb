d0eb1739f85a4836bc776e35925c591b7dd922a4
Tung Nguyen <tongueroo@gmail.com>
2018-02-13 00:32:29 -0800

    allow for environment specific setting file

--- lib/ufo/setting.rb
+++ lib/ufo/setting.rb
@@ -11,15 +11,15 @@ module Ufo
     def data
       return @settings_yaml if @settings_yaml
 
-      if @check_ufo_project && !File.exist?(project_settings_path)
+      if @check_ufo_project && !a_settings_file_exists?
         puts "ERROR: No settings file at #{project_settings_path}.  Are you sure you are in a project with ufo setup?"
         puts "Please create a settings file via: ufo init"
         exit 1
       end
 
-      project = File.exist?(project_settings_path) ?
-                  YAML.load_file(project_settings_path) :
-                  {}
+      # project based settings files
+      env = File.exist?(env_settings_path) ? YAML.load_file(env_settings_path) : {}
+      base = File.exist?(base_settings_path) ? YAML.load_file(base_settings_path) : {}
 
       user_file = "#{ENV['HOME']}/.ufo/settings.yml"
       user = File.exist?(user_file) ? YAML.load_file(user_file) : {}
@@ -27,10 +27,19 @@ module Ufo
       default_file = File.expand_path("../default/settings.yml", __FILE__)
       default = YAML.load_file(default_file)
 
-      @settings_yaml = default.merge(user.merge(project))
+      @settings_yaml = default.merge(user.merge(base.merge(env)))
     end
 
   private
+    # need either the base or env specific settings project file to exist
+    def a_settings_file_exists?
+      File.exist?(project_base_settings_path) || File.exist?(project_settings_path)
+    end
+
+    def project_base_settings_path
+      "#{Ufo.root}/.ufo/settings/base.yml"
+    end
+
     def project_settings_path
       "#{Ufo.root}/.ufo/settings/#{Ufo.env}.yml"
     end
