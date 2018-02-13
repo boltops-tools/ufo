9d16af28cccdb8ebb99a33845da09f0f1bc5b89c
Tung Nguyen <tongueroo@gmail.com>
2018-02-13 01:50:23 -0800

    change key to aws_profiles

--- lib/ufo/core.rb
+++ lib/ufo/core.rb
@@ -31,7 +31,7 @@ module Ufo
     def env_from_profile(aws_profile)
       data = YAML.load_file("#{Ufo.root}/.ufo/settings.yml")
       env = data.find do |_env, setting|
-        profiles = setting['profiles']
+        profiles = setting['aws_profiles']
         profiles && profiles.include?(aws_profile)
       end
       env.first if env
