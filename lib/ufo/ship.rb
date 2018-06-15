eb9b2d9533ff54b64a9a266198872257c312b93c
Tung Nguyen <tongueroo@gmail.com>
2018-06-15 12:23:48 -0700

    slight refactor of ship, sweep dirt into create and update ship modules

--- lib/ufo/ship.rb
+++ lib/ufo/ship.rb
@@ -5,8 +5,13 @@ module Ufo
   class ShipmentOverridden < UfoError; end
 
   class Ship
+    autoload :Create, "ufo/ship/create"
+    autoload :Update, "ufo/ship/update"
+
     include AwsService
     include Util
+    include Create
+    include Update
 
     def initialize(service, task_definition, options={})
       @service = service
@@ -151,62 +156,6 @@ module Ufo
        deployment.desired_count == deployment.running_count)
     end
 
-    # $ aws ecs create-service --generate-cli-skeleton
-    # {
-    #     "cluster": "",
-    #     "serviceName": "",
-    #     "taskDefinition": "",
-    #     "desiredCount": 0,
-    #     "loadBalancers": [
-    #         {
-    #             "targetGroupArn": "",
-    #             "containerName": "",
-    #             "containerPort": 0
-    #         }
-    #     ],
-    #     "role": "",
-    #     "clientToken": "",
-    #     "deploymentConfiguration": {
-    #         "maximumPercent": 0,
-    #         "minimumHealthyPercent": 0
-    #     }
-    # }
-    #
-    # If the service needs to be created it will get created with some default settings.
-    # When does a normal deploy where an update happens only the only thing that ufo
-    # will update is the task_definition.  The other settings should normally be updated with
-    # the ECS console.  `ufo scale` will allow you to updated the desired_count from the
-    # CLI though.
-    def create_service
-      puts "This service #{@service.colorize(:green)} does not yet exist in the #{@cluster.colorize(:green)} cluster.  This deploy will create it."
-      container = container_info(@task_definition)
-      target_group = target_group_prompt(container)
-
-      message = "#{@service} service created on #{@cluster} cluster"
-      if @options[:noop]
-        message = "NOOP #{message}"
-      else
-        options = {
-          cluster: @cluster,
-          service_name: @service,
-          task_definition: @task_definition
-        }
-        options = options.merge(default_params[:create_service])
-        unless target_group.nil? || target_group.empty?
-          add_load_balancer!(container, options, target_group)
-        end
-
-        puts "Creating ECS service with params:"
-        display_params(options)
-        show_aws_cli_command(:create, options)
-        response = ecs.create_service(options)
-        service = response.service # must set service here since this might never be called if @wait_for_deployment is false
-      end
-
-      puts message unless @options[:mute]
-      service
-    end
-
     def show_aws_cli_command(action, params)
       puts "Equivalent aws cli command:"
       # Use .ufo/data instead of .ufo/output because output files all get looped
@@ -222,58 +171,6 @@ module Ufo
 
       file_path = "file://#{rel_path}"
       puts "  aws ecs #{action}-service --cli-input-json #{file_path}".colorize(:green)
-    end
-
-    # $ aws ecs update-service --generate-cli-skeleton
-    # {
-    #     "cluster": "",
-    #     "service": "",
-    #     "taskDefinition": "",
-    #     "desiredCount": 0,
-    #     "deploymentConfiguration": {
-    #         "maximumPercent": 0,
-    #         "minimumHealthyPercent": 0
-    #     }
-    # }
-    # Only thing we want to change is the task-definition
-    def update_service(ecs_service)
-      message = "#{ecs_service.service_name} service updated on #{ecs_service.cluster_name} cluster with task #{@task_definition}"
-      if @options[:noop]
-        message = "NOOP #{message}"
-      else
-        params = {
-          cluster: ecs_service.cluster_arn, # can use the cluster name also since it is unique
-          service: ecs_service.service_arn, # can use the service name also since it is unique
-          task_definition: @task_definition
-        }
-        params = params.merge(default_params[:update_service] || {})
-        puts "Updating ECS service with params:"
-        display_params(params)
-        show_aws_cli_command(:update, params)
-
-        unless @options[:noop]
-          response = ecs.update_service(params)
-          service = response.service # must set service here since this might never be called if @wait_for_deployment is false
-        end
-      end
-
-      puts message unless @options[:mute]
-      service
-    end
-
-    # Only support Application Load Balancer
-    # Think there is an AWS bug that complains about not having the LB
-    # name but you cannot pass both a LB Name and a Target Group.
-    def add_load_balancer!(container, options, target_group)
-      options.merge!(
-        load_balancers: [
-          {
-            container_name: container[:name],
-            container_port: container[:port],
-            target_group_arn: target_group,
-          }
-        ]
-      )
     end
 
     # Returns the target_group.
---
+++ lib/ufo/ship/create.rb
@@ -0,0 +1,74 @@
+class Ufo::Ship
+  module Create
+    # $ aws ecs create-service --generate-cli-skeleton
+    # {
+    #     "cluster": "",
+    #     "serviceName": "",
+    #     "taskDefinition": "",
+    #     "desiredCount": 0,
+    #     "loadBalancers": [
+    #         {
+    #             "targetGroupArn": "",
+    #             "containerName": "",
+    #             "containerPort": 0
+    #         }
+    #     ],
+    #     "role": "",
+    #     "clientToken": "",
+    #     "deploymentConfiguration": {
+    #         "maximumPercent": 0,
+    #         "minimumHealthyPercent": 0
+    #     }
+    # }
+    #
+    # If the service needs to be created it will get created with some default settings.
+    # When does a normal deploy where an update happens only the only thing that ufo
+    # will update is the task_definition.  The other settings should normally be updated with
+    # the ECS console.  `ufo scale` will allow you to updated the desired_count from the
+    # CLI though.
+    def create_service
+      puts "This service #{@service.colorize(:green)} does not yet exist in the #{@cluster.colorize(:green)} cluster.  This deploy will create it."
+      container = container_info(@task_definition)
+      target_group = target_group_prompt(container)
+
+      message = "#{@service} service created on #{@cluster} cluster"
+      if @options[:noop]
+        message = "NOOP #{message}"
+      else
+        options = {
+          cluster: @cluster,
+          service_name: @service,
+          task_definition: @task_definition
+        }
+        options = options.merge(default_params[:create_service])
+        unless target_group.nil? || target_group.empty?
+          add_load_balancer!(container, options, target_group)
+        end
+
+        puts "Creating ECS service with params:"
+        display_params(options)
+        show_aws_cli_command(:create, options)
+        response = ecs.create_service(options)
+        service = response.service # must set service here since this might never be called if @wait_for_deployment is false
+      end
+
+      puts message unless @options[:mute]
+      service
+    end
+
+    # Only support Application Load Balancer
+    # Think there is an AWS bug that complains about not having the LB
+    # name but you cannot pass both a LB Name and a Target Group.
+    def add_load_balancer!(container, options, target_group)
+      options.merge!(
+        load_balancers: [
+          {
+            container_name: container[:name],
+            container_port: container[:port],
+            target_group_arn: target_group,
+          }
+        ]
+      )
+    end
+  end
+end
