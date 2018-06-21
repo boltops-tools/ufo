058423fb2d5d3f388e1da7124cdca2ef2ef7a703
Tung Nguyen <tongueroo@gmail.com>
2018-06-21 14:53:12 -0700

    ufo apps: map cfn info to get pretty service name

--- lib/ufo/apps.rb
+++ lib/ufo/apps.rb
@@ -34,16 +34,80 @@ module Ufo
       table = Text::Table.new
       table.head = ["Service Name", "Task Definition", "Running", "Launch type", "Dns", "Ufo?"]
       resp["services"].each do |s|
-        table.rows << Service.new(s).to_a
+        table.rows << Service.new(s, @options).to_a
       end
       puts table
+    end
+
+    class CfnInfo
+      extend Memoist
+      include Stack::Helper
+
+      def initialize(options = {})
+        @options = options
+        @cluster = @options[:cluster] || default_cluster
+        @map = {}
+      end
+
+      def map
+        return @map if @populated
+
+        populate_map!
+        @populated = true
+        @map
+      end
+
+      def summaries
+        filter = %w[
+          UPDATE_COMPLETE
+          CREATE_COMPLETE
+          UPDATE_ROLLBACK_COMPLETE
+          UPDATE_IN_PROGRESS
+          UPDATE_COMPLETE_CLEANUP_IN_PROGRESS
+          UPDATE_ROLLBACK_IN_PROGRESS
+          UPDATE_ROLLBACK_FAILED
+          UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS
+          REVIEW_IN_PROGRESS
+        ]
+
+        summaries = []
+        next_token = true
+        while next_token
+          resp = cloudformation.list_stacks(stack_status_filter: filter)
+          summaries += resp.stack_summaries
+          next_token = resp.next_token
+        end
+
+        # look for stacks that beling that ufo create
+        summaries.select do |s|
+          s.template_description =~ /Ufo ECS stack/
+        end
+      end
+      memoize :summaries
+
+      def populate_map!
+        summaries.each do |summary|
+          resp = cloudformation.describe_stack_resources(stack_name: summary.stack_name)
+          ecs_resource = resp.stack_resources.find do |resource|
+            resource.logical_resource_id == "Ecs"
+          end
+          # Example: "PhysicalResourceId": "arn:aws:ecs:us-east-1:160619113767:service/dev-hi-web-Ecs-1HRL8Y9F4D1CR"
+          ecs_service_name = ecs_resource.physical_resource_id.split('/').last
+          @map[ecs_service_name] = stack_name_to_service_name(summary.stack_name)
+        end
+      end
+
+      def stack_name_to_service_name(stack_name)
+        stack_name.sub("#{@cluster}-",'')
+      end
     end
 
     class Service
       extend Memoist
 
-      def initialize(service)
+      def initialize(service, options)
         @service = service
+        @options = options
       end
 
       def to_a
@@ -58,12 +122,24 @@ module Ufo
         @service["launch_type"]
       end
 
+      def cfn_map
+        @cfn_info ||= CfnInfo.new(@options)
+        @cfn_info.map
+      end
+
       def ufo?
-        "yes"
+        yes = !!cfn_map[@service["service_name"]]
+        yes ? "yes" : "no"
       end
 
       def name
-        @service["service_name"]
+        full_service_name = @service["service_name"]
+        pretty_name = cfn_map[full_service_name]
+        if pretty_name
+          "#{full_service_name} (#{pretty_name})"
+        else
+          full_service_name
+        end
       end
 
       def dns
