3c2dc761d3da8c99545c862efd1df1fe25ab3d59
Tung Nguyen <tongueroo@gmail.com>
2019-06-13 03:46:17 +0000

    update comment at top of generated Dockerfile

--- lib/ufo/docker/compiler.rb
+++ lib/ufo/docker/compiler.rb
@@ -13,7 +13,10 @@ class Ufo::Docker
       vars = YAML.load_file(path)[Ufo.env] if File.exist?(path)
       vars ||= {}
       result = RenderMePretty.result(@erb_file, vars)
-      comment = "# Note this file was generated from #{File.basename(@erb_file)} as a part of running ufo ship"
+      comment =<<~EOL.chop # remove the trailing newline
+        # Note this file was generated from #{File.basename(@erb_file)} as a part of running ufo ship"
+        # To update the FROM statement with the latest base docker image run: ufo docker base
+      EOL
       result = "#{comment}\n#{result}"
       IO.write(@dockerfile, result)
     end
