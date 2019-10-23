85bbef935904bedc037d67744e6f31484f311d73
Tung Nguyen <tongueroo@gmail.com>
2019-10-23 02:41:42 +0000

    fix ufo_env aws_profile tight binding

--- lib/ufo/core.rb
+++ lib/ufo/core.rb
@@ -42,10 +42,10 @@ module Ufo
       return if ENV['TEST']
       return unless File.exist?("#{Ufo.root}/.ufo/settings.yml") # for rake docs
       return unless settings # Only load if within Ufo project and there's a settings.yml
-      data = settings[Ufo.env] || {}
-      if data["aws_profile"]
-        puts "Using AWS_PROFILE=#{data["aws_profile"]} from UFO_ENV=#{Ufo.env} in config/settings.yml"
-        ENV['AWS_PROFILE'] = data["aws_profile"]
+      data = settings || {}
+      if data[:aws_profile]
+        puts "Using AWS_PROFILE=#{data[:aws_profile]} from UFO_ENV=#{Ufo.env} in config/settings.yml"
+        ENV['AWS_PROFILE'] = data[:aws_profile]
       end
     end
 
