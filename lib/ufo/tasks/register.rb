762dcf28310fd08736671c8201ee81a9ce1519d7
Carl Allen <github@allenofmn.com>
2019-10-30 15:09:06 -0500

    Fix firelens functionality

--- lib/ufo/tasks/register.rb
+++ lib/ufo/tasks/register.rb
@@ -70,15 +70,19 @@ module Ufo
 
       definitions = data[:container_definitions]
       definitions.each_with_index do |definition, i|
-        next unless definition[:log_configuration]
-        options = definition[:log_configuration][:options]
-        next unless options
-
-        # LogConfiguration options do not get transformed and keep their original
-        # structure:
-        #   https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/ECS/Types/ContainerDefinition.html
+        next unless definition[:log_configuration] || definition[:firelens_configuration]
         original_definition = original_data["containerDefinitions"][i]
-        definition[:log_configuration][:options] = original_definition["logConfiguration"]["options"]
+        { log_configuration: 'logConfiguration',
+          firelens_configuration: 'firelensConfiguration' }.each_pair(key, value) do
+
+          next unless definition.dig(key, :options)
+
+          # LogConfiguration and firelensConfiguration options do not get transformed and
+          # keep their original structure:
+          #   https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/ECS/Types/ContainerDefinition.html
+          original_definition = original_data["containerDefinitions"][i]
+          definition[key][:options] = original_definition[value]["options"]
+        end
       end
 
       data
