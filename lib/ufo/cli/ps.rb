f1f09927e42a3e5446352073e589dfe7651e1217
Tung Nguyen <tongueroo@gmail.com>
2022-03-05 19:29:48 +0000

    improve ps errors reporting
    restructure
    add deployment configuration error check

--- lib/ufo/cli/ps.rb
+++ lib/ufo/cli/ps.rb
@@ -25,9 +25,9 @@ class Ufo::CLI
         resp["tasks"]
       end.flatten
 
-      display_tasks(all_task_arns)
-      display_scale_help
-      display_target_group_help
+      tasks = convert_to_task_objects(all_task_arns)
+      show_tasks(tasks)
+      show_errors(tasks)
     end
 
     def summary
@@ -63,50 +63,7 @@ class Ufo::CLI
       resp.scalable_targets.first # scalable_target
     end
 
-    def display_target_group_help
-      events = service["events"][0..4]
-      return if events[0].message =~ /has reached a steady state/
-
-      # The error currently happens to be the 5th element.
-      #
-      # Example:
-      #  "(service XXX) (instance i-XXX) (port 32875) is unhealthy in (target-group arn:aws:elasticloadbalancing:us-east-1:111111111111:targetgroup/devel-Targe-1111111111111/1111111111111111) due to (reason Health checks failed with these codes: [400])">]
-      error_event = events.find do |e|
-        e.message =~ /is unhealthy in/ &&
-        e.message =~ /targetgroup/
-      end
-      return unless error_event
-
-      logger.error "There are targets in the target group reporting unhealthy.  This can cause containers to cycle. Here's the error:"
-      logger.error error_event.message.color(:red)
-      logger.error <<~EOL
-        Check out the ECS console and EC2 Load Balancer console for more info.
-        Sometimes they may not helpful :(
-        Docs that may help: https://ufoships.com/docs/debug/unhealthy-targets/
-      EOL
-    end
-
-    # If the running count less than the desired account yet, check the events
-    # and show a message with helpful debugging information.
-    def display_scale_help
-      return if service.running_count >= service.desired_count
-
-      events = service["events"][0..3] # only check most recent 4 messages
-      error_event = events.find do |e|
-        e.message =~ /was unable to place a task/
-      end
-      return unless error_event
-
-      logger.info "There is an issue scaling the #{@service.color(:green)} service to #{service.desired_count}.  Here's the error:"
-      logger.info error_event.message.color(:red)
-      if service.launch_type == "EC2"
-        logger.info "If AutoScaling is set up for the container instances, it can take a little time to add additional instances. You'll see this message until the capacity is added."
-      end
-    end
-
-    def display_tasks(raw_tasks)
-      raw_tasks.sort_by! { |t| t["task_arn"] }
-      tasks = raw_tasks.map { |t| Task.new(t) } # will have Task objects after this point
+    def show_tasks(tasks)
       tasks = tasks.reject(&:hide?)
       show_notes = show_notes(tasks)
 
@@ -124,6 +81,11 @@ class Ufo::CLI
       presenter.show
     end
 
+    def show_errors(tasks)
+      Errors.new(@options.merge(tasks: tasks)).show
+    end
+
+  private
     def show_notes(tasks)
       tasks.detect { |t| !t.notes.blank? }
     end
@@ -165,6 +127,11 @@ class Ufo::CLI
       end
 
       status == "ALL" ? valid_statuses : [status]
+    end
+
+    def convert_to_task_objects(task_arns)
+      task_arns.sort_by! { |t| t["task_arn"] }
+      task_arns.map { |t| Task.new(t) } # will have Task objects after this point
     end
 
     def task_arns
