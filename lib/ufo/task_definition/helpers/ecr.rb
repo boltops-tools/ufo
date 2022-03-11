e5fa698c5b66459fdb4d4d59ca66bf0afb0b4b10
Tung Nguyen <tongueroo@gmail.com>
2022-03-11 15:32:07 +0000

    warn user when ecr repo not found

--- lib/ufo/task_definition/helpers/ecr.rb
+++ lib/ufo/task_definition/helpers/ecr.rb
@@ -1,13 +1,25 @@
 module Ufo::TaskDefinition::Helpers
   module Ecr
+    include Ufo::Utils::CallLine
+
     def ecr_repo(name)
       repository = ecr_repository(name)
-      repository.repository_uri
+      repository.repository_uri if repository
     end
 
     def ecr_repository(name)
       resp = ecr.describe_repositories(repository_names: [name])
       resp.repositories.first
+    rescue Aws::ECR::Errors::RepositoryNotFoundException => e
+      call_line = ufo_config_call_line
+      logger.warn "WARN: #{e.class} #{e.message}".color(:yellow)
+      logger.warn <<~EOL
+        Called from
+
+            #{call_line}
+
+      EOL
+      nil
     end
   end
 end
