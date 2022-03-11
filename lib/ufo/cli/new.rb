101c1e075a9f5448e9572c7e5b29838462e126ac
Tung Nguyen <tongueroo@gmail.com>
2022-03-11 01:55:37 +0000

    ufo new boot_hook generator

--- lib/ufo/cli/new.rb
+++ lib/ufo/cli/new.rb
@@ -1,5 +1,12 @@
 class Ufo::CLI
   class New < Ufo::Command
+    desc "boot_hook", "Generate boot_hook file"
+    long_desc Help.text("new/boot_hook")
+    BootHook.cli_options.each do |args|
+      option(*args)
+    end
+    register(BootHook, "boot_hook", "boot_hook", "Generate boot_hook file")
+
     desc "helper", "Generate helper file"
     long_desc Help.text("new/helper")
     Helper.cli_options.each do |args|
